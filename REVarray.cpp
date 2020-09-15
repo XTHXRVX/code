2930011f46b396cdbcbecf643a64a3aa600a285d
8veDant <71282147+8veDant@users.noreply.github.com>
2020-09-15 14:01:27 +0530

    Update REVarray.cpp

--- REVarray.cpp
+++ REVarray.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() 
 {
-    int n,i,a[100];
+    int n, i, a[100];
     cout<<"Enter no of elements:";
     cin>>n;
     for (i=0;i<=n-1;i++)
@@ -10,13 +10,19 @@ int main()
     cout<<"Enter a value:";
     cin>>a[i];
 }
-    cout<<"nos entered are"<<endl;
+    cout<<"Nos entered are:"<<endl;
     for ( i=0; i <= n-1; i++)
 {
    cout<<a[i]<<endl;
 }
+    int reverse(int a[],int n);
+    reverse(a,n);
+}
+    int reverse (int a[],int n)
 {
-    int rev[100];
+
+{
+    int i,rev[100];
     for ( i = 0; i <=n-1; i++)
     {
      rev[n-i-1]=a[i];   
@@ -29,5 +35,8 @@ int main()
     }
     
 }
+
+
+
     return 0;
 }
