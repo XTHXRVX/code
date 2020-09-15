4d2385cf6eaedf70ba4b3ddd19fbbb04ae03b18a
8veDant <71282147+8veDant@users.noreply.github.com>
2020-09-15 15:20:11 +0530

    Update REVarray.cpp

--- REVarray.cpp
+++ REVarray.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() 
 {
-    int n, i, a[100];
+    int n,i,a[100];
     cout<<"Enter no of elements:";
     cin>>n;
     for (i=0;i<=n-1;i++)
@@ -10,33 +10,22 @@ int main()
     cout<<"Enter a value:";
     cin>>a[i];
 }
-    cout<<"Nos entered are:"<<endl;
-    for ( i=0; i <= n-1; i++)
-{
-   cout<<a[i]<<endl;
-}
     int reverse(int a[],int n);
     reverse(a,n);
 }
-    int reverse (int a[],int n)
+int reverse(int a[],int n)
 {
-
-{
-    int i,rev[100];
+    int i, rev[100];
     for ( i = 0; i <=n-1; i++)
     {
      rev[n-i-1]=a[i];   
     }
     cout<<"The reverse of this array is:\n";
     for ( i = 0; i<=n-1; i++)
-
     {
         cout<<rev[i]<<endl;
     }
+    return 0;   
+}
     
-}
 
-
-
-    return 0;
-}
