b493da914785c60b271d40be4389ef285726fe64
8veDant <71282147+8veDant@users.noreply.github.com>
2020-09-16 14:09:21 +0530

    Add files via upload

---
+++ REVstring.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+    int n=0;
+    char a[100];
+    cout<<"Enter the statement:";
+    cin<<a;
+    while(a[n]!='\0')
+   {
+     n++;
+   } 
+   int reverse(char a[100],int n);
+   reverse(a,n)
+}
+int reverse(char[100],int n)
+
+return 0;
+}
---
+++ pointerfunc.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+int a,b;
+cout<<"Enter the nos. :";
+cin>>a>>b;
+int sum(int *p1, int *p2);
+sum(&a,&b);
+}
+int su,(int *p1,int *p2);
+{
+    int c;
+    c = *p1+*p2;
+    cout<<"THE sum of the nos. is: ";
+    
+}
+
+return 0;
+}
